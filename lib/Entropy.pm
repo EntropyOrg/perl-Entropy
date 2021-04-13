9176ecf8e4dabcd9e49de5f017b902328c01ecbc
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-04-13 13:36:21 -0500

    Initial commit

---
+++ lib/Entropy.pm
@@ -0,0 +1,18 @@
+package Entropy;
+# ABSTRACT: Namespace for scientific libraries
+
+use strict;
+use warnings;
+
+1;
+
+=head1 DESCRIPTION
+
+B<Entropy> is namespace reserved for modules for science, engineering, and data
+analysis that work together.
+
+=head1 SEE ALSO
+
+L<http://entropyorg.github.io/>, L<https://github.com/EntropyOrg>
+
+=cut
